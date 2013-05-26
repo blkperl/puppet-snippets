0aad97488b7aadfc3f31b2be762a0ee1a996cb6c
William Van Hevelingen <blkperl@cat.pdx.edu>
2013-05-26 07:02:15 +0000

    Add ip and shortnames to sshkeys

--- modules/exported/manifests/sshkeys.pp
+++ modules/exported/manifests/sshkeys.pp
@@ -1,13 +1,15 @@
 class exported::sshkeys {
 
-  @@sshkey { "$::hostname-dsa":
-    type => dsa,
-    key  => $sshdsakey
+  @@sshkey { "${::fqdn}_dsa":
+    host_aliases => [ $::fqdn, $::hostname, $::ipaddress ],
+    type         => dsa,
+    key          => $::sshdsakey,
   }
 
-  @@sshkey { "$::hostname-rsa":
-    type => rsa,
-    key  => $::sshrsakey
+  @@sshkey { "${::fqdn}_rsa":
+    host_aliases => [ $::fqdn, $::hostname, $::ipaddress ],
+    type         => rsa,
+    key          => $::sshrsakey,
   }
 
   # collect all ssh keys
